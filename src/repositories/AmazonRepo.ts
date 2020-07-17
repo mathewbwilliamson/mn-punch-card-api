7ed6b73d9e9c89a7555576376d1b4c47eefa7e12
Matt Williamson <mathewbwilliamson@gmail.com>
2020-07-16 21:43:36 -0400

    Added axios for Amazon call

--- src/repositories/AmazonRepo.ts
+++ src/repositories/AmazonRepo.ts
@@ -2,7 +2,7 @@ import { RawAmazonRequestBody } from '../types/amazonTypes';
 
 export const saveAmazonItem = (amazonItem: RawAmazonRequestBody) => {
     console.log(
-        '\x1b[41m%s \x1b[0m',
+        '\x1b[43m%s \x1b[0m',
         '[matt] SAVEAMAZONITEM amazonItem',
         amazonItem
     );
--- src/services/AmazonService.ts
+++ src/services/AmazonService.ts
@@ -1,32 +1,31 @@
-import unirest from 'unirest';
 import { saveAmazonItem } from '../repositories/AmazonRepo';
 import { RawAmazonRequestBody } from '../types/amazonTypes';
+import axios from 'axios';
 
 export const getAmazonItem = async (asin: string) => {
-    return (await unirest
-        .get('https://amazon-price1.p.rapidapi.com/priceReport')
-        .query({
-            asin,
-            marketplace: 'US',
+    return (
+        await axios({
+            method: 'GET',
+            url: 'https://amazon-price1.p.rapidapi.com/priceReport',
+            headers: {
+                'content-type': 'application/octet-stream',
+                'x-rapidapi-host': 'amazon-price1.p.rapidapi.com',
+                'x-rapidapi-key':
+                    'baa201456amshfc3a4b8c4e76b2bp13db6cjsnc31bfcd08549',
+                useQueryString: true,
+            },
+            params: {
+                asin,
+                marketplace: 'US',
+            },
         })
-        .header({
-            'x-rapidapi-host': 'amazon-price1.p.rapidapi.com',
-            'x-rapidapi-key':
-                'baa201456amshfc3a4b8c4e76b2bp13db6cjsnc31bfcd08549',
-            useQueryString: true,
-        })
-        .end((apiResponse: UnirestResponse<RawAmazonRequestBody>) => {
-            if (apiResponse.error) {
-                throw new Error(apiResponse.error);
-            }
-            return apiResponse.body;
-        })) as UnirestResponse<RawAmazonRequestBody>;
+    ).data as RawAmazonRequestBody;
 };
 
 export const getAndSaveAmazonItem = async (asin: string) => {
     const amazonItem = await getAmazonItem(asin);
 
     // need to call repo and save the item in the DB
-    saveAmazonItem(amazonItem.body);
+    saveAmazonItem(amazonItem);
     return amazonItem;
 };
