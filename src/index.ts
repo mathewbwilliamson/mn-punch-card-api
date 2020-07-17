b6802cb8ea7014d56ef8b572968b8406f5bbb1f3
Matt Williamson <mathewbwilliamson@gmail.com>
2020-07-17 00:51:57 -0400

    Added in cors

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import 'reflect-metadata'; // this shim is required
-import express from 'express';
 import bodyParser from 'body-parser';
 import { createExpressServer } from 'routing-controllers';
-import { ProductsModel } from './config/database-connection';
+import cors from 'cors';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Controller imports
 import { AmazonController } from './controllers/AmazonController';
 
 // creates express app, registers all controller routes and returns you express app instance
 const app = createExpressServer({
+    cors: true,
     controllers: [AmazonController], // we specify controllers we want to use
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(process.env.API_PORT, () => {
+    console.log(`Example app listening on port ${process.env.API_PORT}!`);
 });
